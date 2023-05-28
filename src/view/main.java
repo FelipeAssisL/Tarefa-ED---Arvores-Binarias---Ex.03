45eaf495bd363ee4e0e9d5f751a0899701df2124
Felipe <Felipe@MULTILK>
2023-05-28 14:52:37 -0300

    First Commit

---
+++ src/view/main.java
@@ -0,0 +1,21 @@
+package view;
+
+import br.edu.fateczl.arvorechar.Arvore;
+
+public class main {
+
+	public static void main(String[] args) throws Exception {
+		char[] vet = { 'M', 'F', 'S', 'D', 'J', 'P', 'U', 'A', 'E', 'H', 'Q', 'T', 'W', 'K' };
+		Arvore arvore = new Arvore();
+		for (char i : vet) {
+			arvore.insert(i);
+		}
+		arvore.remove('F');
+		arvore.remove('U');
+		arvore.prefixSearch();
+		System.out.println(" ");
+		arvore.postfixSearch();
+		System.out.println(" ");
+		arvore.infixSearch();
+	}
+}
